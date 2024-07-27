237a429710c09ce9904274fb8a6d80252c811555
nhphuc <nhphuc@selab1-enp3s0>
2024-07-27 11:01:56 +0000

    second commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,28 @@ const fs = require('fs');
 const {spawn} = require('child_process');
 const directoryGalleryPath = 'public/Image/Gallery';
 var router = express.Router();
+const axios = require('axios');
+const Jimp = require('jimp');
 
 var prompt = "";
+const server_py_port = '20213'
+const server_generate = '172.29.64.75'
+const server_generate_port = '8000'
+
+const input_dir = 'public/Image/Paint/Input/input.png'
+const mask_dir = 'public/Image/Paint/Mask/mask.png'
+const output_dir = 'public/Image/Paint/Output/output.png'
+const output_gallery_dir = `public/Image/Gallery/z_output${Date.now()}.png`
+
+var importSize = {
+  width: 0,
+  height: 0,
+}
+
+var offsetSize = {
+  widthOffset: 0,
+  heightOffset: 0,
+}
 
 const galleryStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -38,67 +58,253 @@ const galleryUpload = multer({ storage: galleryStorage });
 const paintInputUpload = multer({ storage: paintInputStorage });
 const paintMaskUpload = multer({ storage: paintMaskStorage });
 
-/* GET home page. */
-router.post('/image-sketch', function(req, res, next) {
+router.post('/image_generation', async function(req, res, next) {
   console.log(req.body.text);
-  const python = spawn('python', [
-    'D:/moving.py',
-  ]);
-  python.on('close', (code) => {
-    res.send(code.toString());
-  });
+  /*fs.unlink(output_dir, (err) => {
+    if (err) {
+        console.error('Error deleting the file:', err);
+    } else {
+        console.log('File deleted successfully!');
+    }
+  });*/
+  const data = {text: req.body.text};
+  axios.post(`http://0.0.0.0:${server_py_port}/image-generation`, data).then((response) => {
+    fs.copyFileSync(output_dir, output_gallery_dir);
+    res.send(response.text);
+  })
 });
 
-router.post('/image-generation', function(req, res, next) {
-  console.log(req.body.text);
-  const python = spawn('python', [
-    'D:/moving.py',
-  ]);
-  python.on('close', (code) => {
-    res.send(code.toString());
-  });
+router.post('/add_item', async function(req, res, next) {
+  //read Image
+  const imageFile = fs.readFileSync(input_dir);
+  const imageBase64String = Buffer.from(imageFile).toString('base64');
+
+  //read Mask
+  const maskFile = fs.readFileSync(mask_dir)
+  const maskBase64String = Buffer.from(maskFile).toString('base64');
+
+  const data = {
+    image: imageBase64String,
+    mask: maskBase64String,
+    prompt: req.body.text,
+  };
+  console.log(data)
+  await axios.post(`http://${server_generate}:${server_generate_port}/add_item`, data).then((response) => {
+    console.log(response.data);
+    fs.writeFileSync(output_dir, response.data.data, { encoding: 'base64' }, (err) => {
+      if (err) {
+          console.error('Error writing file:', err);
+      } else {
+          console.log('Image saved');
+      }
+    });
+    fs.copyFileSync(output_dir, output_gallery_dir);
+    res.send(response.data);
+  })
 });
 
-router.post('/object-adjustment', function(req, res, next) {
-  console.log(req.body.text);
-  const python = spawn('python', [
-    'D:/moving.py',
-  ]);
-  python.on('close', (code) => {
-    res.send(code.toString());
-  });
+router.post('/alter_background', async function(req, res, next) {
+  //read Image
+  const imageFile = fs.readFileSync(input_dir);
+  const imageBase64String = Buffer.from(imageFile).toString('base64');
+
+  //read Mask
+  const maskFile = fs.readFileSync(mask_dir)
+  const maskBase64String = Buffer.from(maskFile).toString('base64');
+
+  const data = {
+    image: imageBase64String,
+    mask: maskBase64String,
+    prompt: req.body.text,
+  };
+  console.log(data)
+  await axios.post(`http://${server_generate}:${server_generate_port}/alter_background`, data).then((response) => {
+    console.log(response.data);
+    fs.writeFileSync(output_dir, response.data.data, { encoding: 'base64' }, (err) => {
+      if (err) {
+          console.error('Error writing file:', err);
+      } else {
+          console.log('Image saved');
+      }
+    });
+    fs.copyFileSync(output_dir, output_gallery_dir);
+    res.send(response.data);
+  })
 });
 
-router.post('/object-addition', function(req, res, next) {
-  console.log(req.body.text);
-  const python = spawn('python', [
-    'D:/moving.py',
-  ]);
-  python.on('close', (code) => {
-    res.send(code.toString());
-  });
+router.post('/change_pose_view', async function(req, res, next) {
+  //read Image
+  const imageFile = fs.readFileSync(input_dir);
+  const imageBase64String = Buffer.from(imageFile).toString('base64');
+
+  //read Mask
+  const maskFile = fs.readFileSync(mask_dir)
+  const maskBase64String = Buffer.from(maskFile).toString('base64');
+
+  const data = {
+    image: imageBase64String,
+    mask: maskBase64String,
+    prompt: req.body.text,
+  };
+  console.log(data)
+  await axios.post(`http://${server_generate}:${server_generate_port}/change_pose_view`, data).then((response) => {
+    console.log(response.data);
+    fs.writeFileSync(output_dir, response.data.data, { encoding: 'base64' }, (err) => {
+      if (err) {
+          console.error('Error writing file:', err);
+      } else {
+          console.log('Image saved');
+      }
+    });
+    fs.copyFileSync(output_dir, output_gallery_dir);
+    res.send(response.data);
+  })
 });
 
-router.post('/concept-generation', function(req, res, next) {
-  console.log(req.body.text);
-  const python = spawn('python', [
-    'D:/moving.py',
-  ]);
-  python.on('close', (code) => {
-    res.send(code.toString());
-  });
+router.post('/replace_object_fixed', async function(req, res, next) {
+  //read Image
+  const imageFile = fs.readFileSync(input_dir);
+  const imageBase64String = Buffer.from(imageFile).toString('base64');
+
+  //read Mask
+  const maskFile = fs.readFileSync(mask_dir)
+  const maskBase64String = Buffer.from(maskFile).toString('base64');
+
+  const data = {
+    image: imageBase64String,
+    mask: maskBase64String,
+    prompt: req.body.text,
+  };
+  console.log(data)
+  await axios.post(`http://${server_generate}:${server_generate_port}/replace_object_fixed`, data).then((response) => {
+    console.log(response.data);
+    fs.writeFileSync(output_dir, response.data.data, { encoding: 'base64' }, (err) => {
+      if (err) {
+          console.error('Error writing file:', err);
+      } else {
+          console.log('Image saved');
+      }
+    });
+    fs.copyFileSync(output_dir, output_gallery_dir);
+    res.send(response.data);
+  })
 });
 
-router.post('/pose-transition', function(req, res, next) {
-  console.log(req.body.text);
-  const python = spawn('python', [
-    'D:/moving.py',
-  ]);
-  python.on('close', (code) => {
-    res.send(code.toString());
-  });
+router.post('/replace_object_dynamic', async function(req, res, next) {
+  //read Image
+  const imageFile = fs.readFileSync(input_dir);
+  const imageBase64String = Buffer.from(imageFile).toString('base64');
+
+  //read Mask
+  const maskFile = fs.readFileSync(mask_dir)
+  const maskBase64String = Buffer.from(maskFile).toString('base64');
+
+  const data = {
+    image: imageBase64String,
+    mask: maskBase64String,
+    prompt: req.body.text,
+  };
+  console.log(data)
+  await axios.post(`http://${server_generate}:${server_generate_port}/replace_object_dynamic`, data).then((response) => {
+    console.log(response.data);
+    fs.writeFileSync(output_dir, response.data.data, { encoding: 'base64' }, (err) => {
+      if (err) {
+          console.error('Error writing file:', err);
+      } else {
+          console.log('Image saved');
+      }
+    });
+    fs.copyFileSync(output_dir, output_gallery_dir);
+    res.send(response.data);
+  })
 });
 
+router.post('/thematic_collection_fixed', async function(req, res, next) {
+  //read Image
+  const imageFile = fs.readFileSync(input_dir);
+  const imageBase64String = Buffer.from(imageFile).toString('base64');
+
+  //read Mask
+  const maskFile = fs.readFileSync(mask_dir)
+  const maskBase64String = Buffer.from(maskFile).toString('base64');
+
+  const data = {
+    image: imageBase64String,
+    mask: maskBase64String,
+    prompt: req.body.text,
+  };
+  console.log(data)
+  await axios.post(`http://${server_generate}:${server_generate_port}/thematic_collection_fixed`, data).then((response) => {
+    console.log(response.data);
+    fs.writeFileSync(output_dir, response.data.data, { encoding: 'base64' }, (err) => {
+      if (err) {
+          console.error('Error writing file:', err);
+      } else {
+          console.log('Image saved');
+      }
+    });
+    fs.copyFileSync(output_dir, output_gallery_dir);
+    res.send(response.data);
+  })
+});
+
+router.post('/thematic_collection', async function(req, res, next) {
+  //read Image
+  const imageFile = fs.readFileSync(input_dir);
+  const imageBase64String = Buffer.from(imageFile).toString('base64');
+
+  //read Mask
+  const maskFile = fs.readFileSync(mask_dir)
+  const maskBase64String = Buffer.from(maskFile).toString('base64');
+
+  const data = {
+    image: imageBase64String,
+    mask: maskBase64String,
+    prompt: req.body.text,
+  };
+  console.log(data)
+  await axios.post(`http://${server_generate}:${server_generate_port}/thematic_collection`, data).then((response) => {
+    console.log(response.data);
+    fs.writeFileSync(output_dir, response.data.data, { encoding: 'base64' }, (err) => {
+      if (err) {
+          console.error('Error writing file:', err);
+      } else {
+          console.log('Image saved');
+      }
+    });
+    fs.copyFileSync(output_dir, output_gallery_dir);
+    res.send(response.data);
+  })
+});
+
+router.post('/remove_object', async function(req, res, next) {
+  //read Image
+  const imageFile = fs.readFileSync(input_dir);
+  const imageBase64String = Buffer.from(imageFile).toString('base64');
+
+  //read Mask
+  const maskFile = fs.readFileSync(mask_dir)
+  const maskBase64String = Buffer.from(maskFile).toString('base64');
+
+  const data = {
+    image: imageBase64String,
+    mask: maskBase64String,
+    prompt: req.body.text,
+  };
+  console.log(data)
+  await axios.post(`http://${server_generate}:${server_generate_port}/remove_object`, data).then((response) => {
+    console.log(response.data);
+    fs.writeFileSync(output_dir, response.data.data, { encoding: 'base64' }, (err) => {
+      if (err) {
+          console.error('Error writing file:', err);
+      } else {
+          console.log('Image saved');
+      }
+    });
+    fs.copyFileSync(output_dir, output_gallery_dir);
+    res.send(response.data);
+  })
+});
 
 router.get('/galleryName', function(req, res, next) {
   var arrayName = [];
@@ -116,15 +322,51 @@ router.post('/upload', galleryUpload.single('file'), function(req, res, next) {
   res.status(200).send(arrayName);
 });
 
-router.post('/input', paintInputUpload.single('file'), function(req, res, next) {
-
+router.post('/input', paintInputUpload.single('file'), async function(req, res, next) {
+  await Jimp.read(input_dir, async (err, image) => {
+    if (err) throw err;
+    const width = image.bitmap.width;
+    const height = image.bitmap.height;
+    if(importSize.width === 0){
+      importSize.width = width
+      importSize.height = height
+      offsetSize.widthOffset = 66;
+      offsetSize.heightOffset = 66;
+    }
+    await image.crop((width - importSize.width)/2 + offsetSize.widthOffset, (height - importSize.height)/2 + offsetSize.heightOffset, importSize.width - 2 * offsetSize.widthOffset, importSize.height - 2 * offsetSize.heightOffset).write('public/Image/Paint/Input/input.png');
+    res.send('inputDone');
+    const imageFile = fs.readFileSync(input_dir);
+  });
 });
 
-router.post('/mask', paintMaskUpload.single('file'), function(req, res, next) {
-
+router.post('/mask', paintMaskUpload.single('file'), async function(req, res, next) {
+  await Jimp.read(mask_dir, async (err, image) => {
+    if (err) throw err;
+    const width = image.bitmap.width;
+    const height = image.bitmap.height;
+    if(importSize.width === 0){
+      importSize.width = width
+      importSize.height = height
+      offsetSize.widthOffset = 66;
+      offsetSize.heightOffset = 66;
+    }
+    await image.crop((width - importSize.width)/2 + offsetSize.widthOffset, (height - importSize.height)/2 + offsetSize.heightOffset, importSize.width - 2 * offsetSize.widthOffset, importSize.height - 2 * offsetSize.heightOffset).greyscale().normalize().invert().threshold({ max: 10 }).write('public/Image/Paint/Mask/mask.png');
+    res.send('maskDone');
+  });
 });
 
-router.post('/mask', paintMaskUpload.single('file'), function(req, res, next) {
+router.post('/import-size', function(req, res, next) {
+  importSize.width = req.body.width
+  importSize.height = req.body.height
+
+  offsetSize.widthOffset = 66;
+  offsetSize.heightOffset = 66;
+
+  if(importSize.width > importSize.height){
+    offsetSize.widthOffset = offsetSize.widthOffset * (importSize.width/importSize.height)
+  }else if(importSize.height > importSize.width){
+    offsetSize.heightOffset = offsetSize.heightOffset * (importSize.height/importSize.width)
+  }
 
 });
 
